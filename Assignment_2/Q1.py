5438d0e987b5e976d423daf53472a14d01bba400
Utsav Parekh <parekhutsav13@gmail.com>
2020-06-07 13:03:26 +0530

    Second Assignment Commit

--- Assignment_2/Q1.py
+++ Assignment_2/Q1.py
@@ -1,6 +1,7 @@
 import cv2
 
 img= cv2.imread('E:/Robocon Internship/Git/Robocon_Internship/Assignment_2/bgimg.jpg')
+print(img.shape)
 cv2.line(img,(0,0),(956,1300),(255,0,0),3)
 cv2.imshow('Image',img)
 cv2.waitKey(0)
